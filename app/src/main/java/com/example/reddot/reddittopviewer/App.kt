2702b62e3d2ae1e3436b8ed1826636c3c4d191b7
Litvinenko_Anton <litvinenko.anton.89@gmail.com>
2018-11-22 01:25:20 +0200

    add AppSpector remove GoogleChrome CustomTabsIntent (64k methods)

--- app/src/main/java/com/example/reddot/reddittopviewer/App.kt
+++ app/src/main/java/com/example/reddot/reddittopviewer/App.kt
@@ -2,6 +2,7 @@ package com.example.reddot.reddittopviewer
 
 import android.app.Application
 import android.content.Context
+import com.appspector.sdk.AppSpector
 import com.arellomobile.mvp.MvpFacade
 
 class App : Application() {
@@ -16,7 +17,14 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MvpFacade.init()
+
+        MvpFacade
+                .init()
+
+        AppSpector
+                .build(this)
+                .withDefaultMonitors()
+                .run("YjQwN2M1NmItOGRlYy00YTY2LTg3OWQtMGIxOTI1ODgwNTBi")
     }
 
 
--- app/src/main/java/com/example/reddot/reddittopviewer/tools/extensions/ActivityEx.kt
+++ app/src/main/java/com/example/reddot/reddittopviewer/tools/extensions/ActivityEx.kt
@@ -1,8 +1,6 @@
 
 import android.app.Activity
-import android.net.Uri
 import android.support.annotation.StringRes
-import android.support.customtabs.CustomTabsIntent
 
 fun Activity.showToast(@StringRes resId: Int) = applicationContext.showToast(resId)
 
@@ -10,8 +8,6 @@ fun Activity.showToast(message: String?) = applicationContext.showToast(message)
 
 fun Activity.openChromeCustomTabs(url: String?) {
     url?.let {
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this, Uri.parse(url))
+        url.toString()
     }
 }
